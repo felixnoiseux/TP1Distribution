76ecdf8ec30100789ac4ad64d8a3218b0a75f8ba
Unknown <sam7945@hotmail.com>
2019-09-18 15:07:31 -0400

    Pagination réglé problème d'affichage

--- movieGEN/movieGEN/Controllers/HomeController.cs
+++ movieGEN/movieGEN/Controllers/HomeController.cs
@@ -30,23 +30,51 @@ namespace movieGEN.Controllers
                 using (WebClient wc = new WebClient())
                 {
                     var json = wc.DownloadString(url);
-                    if (json != "{\"Response\":\"False\",\"Error\":\"Movie not found!\"}")
+                    if (json != "{\"Response\":\"False\",\"Error\":\"Movie not found!\"}" && json != "{\"Response\":\"False\",\"Error\":\"Too many results.\"}")
                     {
-                        char[] s1 = new char[100];
-                        int i1 = 0;
-                        for (int i = json.Length-1; i > json.Length - 50; i--)
+                        //char[] s1 = new char[100];
+                        //int i1 = 0;
+                        //for (int i = json.Length-1; i > json.Length - 50; i--)
+                        //{
+                        //    s1[i1] = json[i];
+                        //    i1++;
+                        //}
+                        int NbResultat = 0;
+                        if (Int32.TryParse(json.Substring(json.Length - 24, 4), out int x))
                         {
-                            s1[i1] = json[i];
-                            i1++;
+                            NbResultat = x;
+
+                        }
+                        else if (Int32.TryParse(json.Substring(json.Length - 23, 3), out int y))
+                        {
+                            NbResultat = y;
+                        }
+                        else if (Int32.TryParse(json.Substring(json.Length - 22, 2), out int z))
+                        {
+                            NbResultat = z;
+                        }
+                        else if (Int32.TryParse(json.Substring(json.Length - 21, 1), out int z1))
+                        {
+                            NbResultat = z1;
                         }
                         string sFinjson = json.Substring(json.Length - 50, 50);
+                        float nb = NbResultat / 10.0f;
+                        int NPage = NbResultat / 10;
+                        if (nb % 1 != 0)
+                        {
+                            NPage++;
+                        }
+
                         int posArrayFin = json.Length - 50 + sFinjson.IndexOf("]");
-                        if (json[posArrayFin+1] == ',')
+                        if (json[posArrayFin + 1] == ',')
                         {
                             string s = json.Substring(10, posArrayFin - 9);
                             var jsonArray = JArray.Parse(s);
                             lstObj = jsonArray.ToObject<List<ImdbEntity>>();
-                            ViewData["NextPage"] = page + 1;
+                            if (page < NPage)
+                            {
+                                ViewData["NextPage"] = page + 1;
+                            }
                             if (page != 1)
                             {
                                 ViewData["PreviousPage"] = page - 1;
@@ -104,7 +132,8 @@ namespace movieGEN.Controllers
                 "http://www.omdbapi.com/?apikey=c8f45984&t=Fast%20&%20Furious%206&page=1",
                 "http://www.omdbapi.com/?apikey=c8f45984&t=Blindspot&page=1",
                 "http://www.omdbapi.com/?apikey=c8f45984&t=Home%20Alone&page=1",
-                "http://www.omdbapi.com/?apikey=c8f45984&t=Bourne&page=1"
+                "http://www.omdbapi.com/?apikey=c8f45984&t=Bourne&page=1",
+                "http://www.omdbapi.com/?apikey=c8f45984&t=avengers&page=1"
             };
 
             foreach (var url in URL)
